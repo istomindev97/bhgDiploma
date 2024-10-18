c076a494ab06d278b8a551a571ac42a1a4301578
Ilya Istomin <istominilya97@gmail.com>
2024-10-18 15:00:37 +0300

    initial commit

---
+++ public/js/api/Entity.js
@@ -0,0 +1,53 @@
+/**
+ * Класс Entity - базовый для взаимодействия с сервером.
+ * Имеет свойство URL, равно пустой строке.
+ * */
+class Entity {
+  static URL = '';
+  /**
+   * Запрашивает с сервера список данных.
+   * Это могут быть счета или доходы/расходы
+   * (в зависимости от того, что наследуется от Entity)
+   * */
+  static list(data, callback){
+    createRequest({
+      url: this.URL,
+      data,
+      method: "GET",
+      callback: (err, response) => {
+        callback(err, response);
+      }
+    })
+  }
+
+  /**
+   * Создаёт счёт или доход/расход с помощью запроса
+   * на сервер. (в зависимости от того,
+   * что наследуется от Entity)
+   * */
+  static create(data, callback) {
+    createRequest({
+      url: this.URL,
+      data,
+      method: "PUT",
+      callback: (err, response) => {
+        callback(err, response);
+      }
+    })
+  }
+
+  /* *
+   * Удаляет информацию о счёте или доходе/расходе
+   * (в зависимости от того, что наследуется от Entity)
+   * */ 
+  static remove(data, callback ) {
+    createRequest({
+      url: this.URL,
+      data,
+      method: "DELETE",
+      callback: (err, response) => {
+        callback(err, response);
+      }
+    })
+  }
+}
